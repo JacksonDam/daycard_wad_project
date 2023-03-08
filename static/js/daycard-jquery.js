5b1b46414571a04c8f163e2bd9d8a9a8380af3a4
Jackson Dam <2619114d@student.gla.ac.uk>
2023-03-08 23:56:05 +0000

    further card flipping work

--- static/js/daycard-jquery.js
+++ static/js/daycard-jquery.js
@@ -1,10 +1,26 @@
 $(document).ready(function() {
-    console.log("READY");
     const cards = $('.card');
     cards.each(function(i, obj) {
-       obj.addEventListener("click", function() {
-           console.log("FLIP");
-           cards.toggleClass("active");
+        obj.addEventListener("click", function() {
+            $(obj).toggleClass("active");
+            $(this).fadeTo(200, 1);
+            var otherCards = cards.not(this);
+            otherCards.each(function(j, obj2) {
+                $(this).toggleClass("active", false);
+                $(this).fadeTo(200, 0.5);
+            });
        });
+    });
+    $(document).click(function(e) {
+        console.log(e.target);
+        if (!$( ".card" ).is(e.target) && !$( ".card" ).has(e.target).length) {
+            cards.each(function(i, obj) {
+                $(obj).toggleClass("active", false);
+                $(obj).fadeTo(200, 1);
+            });
+        }
+        else {
+            console.log(e.target);
+        };
     });
 });
